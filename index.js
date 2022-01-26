1a2467ddeb9aaabac22fbf677997b8b66833f134
Esther Kang <eskang87@gmail.com>
2022-01-26 15:45:45 -0800

    commit tree 092747472d6f2370c9b16ddc0b3ec88cd6dd19af

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.substr(0, 1).toUpperCase() + string.substr(1)
+  };
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z0-9 '-]/g, '')
+  };
+
+  static titleize(string){
+    let newString = string.split(' ').map(word => {
+      if(word === 'the' || word === 'a' || word === 'an' || word === 'but'|| word === 'of' || word === 'and' || word === 'for' || word === 'at' || word === 'by'|| word === 'from'){
+      return word.toLowerCase()
+      } else {
+      return word.substr(0, 1).toUpperCase() + word.substr(1)
+      }
+      }).join(' ');
+    
+    return newString.substr(0, 1).toUpperCase() + newString.substr(1)
+  };
+
 }
